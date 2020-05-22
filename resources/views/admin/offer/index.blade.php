e706b6486ac42acffa3d6f3b0b8b8913f7b56f90
michael_wagdy <michaelwagdy96@outlook.com>
2020-05-22 14:48:42 +0200

    create admin login api controller

--- resources/views/admin/offer/index.blade.php
+++ resources/views/admin/offer/index.blade.php
@@ -4,6 +4,16 @@
 
 @section('content_header')
     <h1>Offers Managment</h1>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 @stop
 
 @section('content')
--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/admin/login', 'Api\AdminController@login');
+
 Route::group(['middleware'=>'auth:api'], function(){
 Route::get('/category','Api\CategoryManagementController@index');
 Route::get('/category/{id}','Api\CategoryManagementController@show');
 Route::post('/category/create','Api\CategoryManagementController@store');
 Route::patch('/category/update/{id}','Api\CategoryManagementController@update');
 Route::delete('/category/{id}','Api\CategoryManagementController@destroy');
+Route::get('admin/logout', 'Api\AdminController@logout');
+Route::get('admin', 'Api\AdminController@admin');
 });
